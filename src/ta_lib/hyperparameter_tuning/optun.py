7c759466501579123c55f894236454b5a77bcd1b
paduruvignatejreddy <paduru.vignatej@tigeranalytics.com>
2024-12-11 15:02:23 +0530

    used MLflowCallback instead of yield generator

--- src/ta_lib/hyperparameter_tuning/optun.py
+++ src/ta_lib/hyperparameter_tuning/optun.py
@@ -11,6 +11,7 @@ from ta_lib.core.api import (
 )
 from ta_lib.core.api import save_pipeline
 import mlflow
+from optuna.integration.mlflow import MLflowCallback
 logger = logging.getLogger(__name__)
 
 
@@ -41,25 +42,25 @@ def objective(trial, train_X, train_y):
     float
         The R^2 score of the trained RandomForestRegressor model on the training data.
     """
-    with mlflow.start_run(nested=True):
-        mlflow.set_tag("mlflow.runName", str(f"run_{next(gen)}"))
-        n_estimators = trial.suggest_int("n_estimators", 10, 200)
-        max_depth = trial.suggest_int("max_depth", 1, 32)
-        min_samples_split = trial.suggest_int("min_samples_split", 2, 20)
-        min_samples_leaf = trial.suggest_int("min_samples_leaf", 1, 20)
+    # with mlflow.start_run(nested=True):
+    # mlflow.set_tag("mlflow.runName", str(f"run_{next(gen)}"))
+    n_estimators = trial.suggest_int("n_estimators", 10, 200)
+    max_depth = trial.suggest_int("max_depth", 1, 32)
+    min_samples_split = trial.suggest_int("min_samples_split", 2, 20)
+    min_samples_leaf = trial.suggest_int("min_samples_leaf", 1, 20)
 
-        model = RandomForestRegressor(
-            n_estimators=n_estimators,
-            max_depth=max_depth,
-            min_samples_split=min_samples_split,
-            min_samples_leaf=min_samples_leaf,
-            random_state=42
-        )
+    model = RandomForestRegressor(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        min_samples_split=min_samples_split,
+        min_samples_leaf=min_samples_leaf,
+        random_state=42
+    )
 
-        model.fit(train_X, train_y.values.ravel())
-        score = model.score(train_X, train_y)
-        mlflow.log_params(trial.params)
-        mlflow.log_metric("score", score)
+    model.fit(train_X, train_y.values.ravel())
+    score = model.score(train_X, train_y)
+    # mlflow.log_params(trial.params)
+    # mlflow.log_metric("score", score)
     return score
 
 
@@ -88,8 +89,18 @@ def optimization(context):
         features_transformer.fit_transform(train_X, train_y),
         get_feature_names_from_column_transformer(features_transformer),
     )
+    mlflow_callback = MLflowCallback(
+        tracking_uri=mlflow.get_tracking_uri(),
+        metric_name="validation_score",
+        create_experiment=False,
+        mlflow_kwargs={"nested": True}
+    )
     study = optuna.create_study(direction="maximize")
-    study.optimize(lambda trial: objective(trial, train_X_transformed, train_y), n_trials=10)
+    study.optimize(
+        lambda trial: objective(trial, train_X_transformed, train_y), 
+        n_trials=10,
+        callbacks=[mlflow_callback]
+    )
     best_trial = study.best_trial
     logger.info(f"Best hyperparameters: {best_trial.params}")
     best_model = RandomForestRegressor(
@@ -103,9 +114,9 @@ def optimization(context):
     model_filename = op.join(artifacts_folder, "random_forest_model.joblib")
     save_pipeline(best_model, model_filename)
 
-    with mlflow.start_run(nested=True):
-        mlflow.set_tag("mlflow.runName", "best_model")
-        mlflow.log_params(best_trial.params)
-        mlflow.log_metric("best_score", best_trial.value)
-        mlflow.log_artifact(model_filename)
+    # with mlflow.start_run(nested=True):
+    # mlflow.set_tag("mlflow.runName", "best_model")
+    mlflow.log_params(best_trial.params)
+    mlflow.log_metric("best_score", best_trial.value)
+    mlflow.log_artifact(model_filename)
     return best_model
